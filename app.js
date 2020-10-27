ff4664c5507b401bda582dfa435a5fd4d14bc3ef
GitSkynet <carlos.curtido@gmail.com>
2020-10-27 17:19:38 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
+
+//bcryptjs, salt rounds y salt
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+////
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// require database configuration
+require('./configs/db.config');
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    resave: true,
+    saveUninitialized: false
+  }));
+
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+var authRouter = require('./routes/auth');
+var index = require('./routes/index');
+
+app.use('/', authRouter);
+app.use('/', index);
+
+
+module.exports = app;
---
+++ configs/db.config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+  mongoose
+  .connect("mongodb://localhost/Proyecto2", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
