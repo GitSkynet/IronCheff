870c62d49cd8621aee12d65d1b5f8b2cd1139f83
GitSkynet <carlos.curtido@gmail.com>
2020-10-31 13:42:58 +0100

    Git commit Sabado 13:42

--- models/Recipe.js
+++ models/Recipe.js
@@ -4,13 +4,13 @@ const Schema = mongoose.Schema;
 const recetasSchema = new Schema(
     {
       name: String,
-      ingredients: [{type: String}], //[{type: Schema.Types.ObjectId, ref: 'Ingredients'}],
+      ingredients: String,
       instructions: String,
       cuisine: String,
       image: {type: String, default: 'images.media-allrecipes.com/images/75131.jpg'},
       diners: Number,
       score: Number,
-      creator: String, //{type: Schema.Types.ObjectId, ref: 'User'},
+      creator: {type: Schema.Types.ObjectId, ref: 'User'},
       idScorer: Number
     });
     const Recipe = mongoose.model('Recipes', recetasSchema);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require("express");
 var router = express.Router();
 const Recipe = require('../models/Recipe');
 
-
-
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("home");
